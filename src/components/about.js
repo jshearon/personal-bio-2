d9406af6aa6e0e9256f17bb4822dc437f75c9de8
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-11-30 19:28:02 -0600

    working slider for projects

--- src/components/about.js
+++ src/components/about.js
@@ -13,8 +13,18 @@ export const About = () => {
   })
 
   return (
-    <div className="about">
-      About
+    <div className="about text-left m-auto">
+      <h1>Hi, I'm Jonathan. </h1>
+      
+      <h3>I'm a full-stack software developer who loves to make cool things 
+      that make life easier for me and everyone else. Things that get business done and make productivity soar.</h3> 
+      <h3>I'm currently enrolled in Nashville Software School
+      to level up my self-taught programming skills. I speak fluent web stack, including HTML5, Javascript ES6, CSS/Sass, Bootstrap, React, Python, Django, PHP - all the good stuff. 
+      I know Linux, the command line and 
+      </h3>
+      <h3>
+      In my spare time I enjoy writing orchestral music, playing classical guitar and spending time with my amazing wife and two kids.  
+      </h3>
     </div>
   )
 }
