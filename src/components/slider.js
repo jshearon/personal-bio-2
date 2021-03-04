c2ced57ca93b2626cc820e5be0c35309e6ee87a8
Jonathan Shearon <jonathan.shearon@gmail.com>
2021-03-04 13:13:10 -0600

    feature complete

--- src/components/slider.js
+++ src/components/slider.js
@@ -28,10 +28,10 @@ export default function SimpleSlider() {
         <div className="project-text text-left">
           <h2 className="project-title">{project.title}</h2>
             <p className="project-description">{project.description}</p>
-            <div className="d-flex flex-row justify-content-between align-items-center">
+            <div className="d-flex flex-row flex-wrap justify-content-start align-items-center">
               <div className="mr-3 project-links"><a className="m-auto" href={project.code}><FontAwesomeIcon icon={faGithub} /> Code</a></div>
               <div className="mr-3 project-links"><a className="m-auto" href={project.url}><FontAwesomeIcon icon={faChrome} /> Demo</a></div>
-              <div className="project-badges d-flex flex-row justify-content-between w-50 ml-auto">
+              <div className="project-badges d-flex flex-row justify-content-between w-100 mt-3">
                 {
                   project.tech.map((technology) => {
                     return (<img src={`/images/${technology}.svg`} alt="tech badge" className="techbadge" />)
